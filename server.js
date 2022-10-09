9188b627f8cb8d4a40e6ea2c60130c8ae987a7f0
lohwenxin <e0563995@u.nus.edu>
2022-10-09 18:02:17 +0800

    Script for server and client sockets for live chat app

---
+++ server.js
@@ -0,0 +1,20 @@
+const io = require("socket.io")(3000, {
+    cors: {
+      origin: "*",
+    },
+  });
+const users = {}
+
+io.on('connection', socket => {
+    socket.on('new-user', username => {
+        users[socket.id] = username
+        socket.broadcast.emit('user-connected', username)
+    })
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', { message: message, username: users[socket.id] })
+    })
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
+})
